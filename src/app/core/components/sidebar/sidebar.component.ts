863253aca79c1153afe5df6f7ff65bb4b3f69a51
Pedro Tomilhero <ptomilhero27@gmail.com>
2024-06-23 08:53:20 -0300

    feat: create sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LucideAngularModule, icons } from 'lucide-angular';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, LucideAngularModule, MenuModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  public items: MenuItem[] = [];
 
+  ngOnInit() {
+    this.items = [
+      { label: 'Cards', icon: 'credit-card', routerLink: ['/cards'] },
+      { label: 'Sets', icon: 'grid', routerLink: ['/sets'] },
+      { label: 'Favorites', icon: 'star', routerLink: ['/favorites'] }
+    ];
+  }
 }
